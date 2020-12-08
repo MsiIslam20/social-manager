f1f581a1e6c9c4aacf5ad8280bc6898878e79909
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-12-08 21:11:51 +0600

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Container, Row, Col} from 'react-bootstrap';
+import Form from '../Forms/Form';
+import Posts from '../Posts/Posts';
 
 const Home = () => {
     return (
         <Container>
             <Row>
                 <Col md={7}>
-                    
+                    <Posts></Posts>
                 </Col>
                 <Col md={5}>
-
+                    <Form></Form>
                 </Col>
             </Row>
         </Container>
